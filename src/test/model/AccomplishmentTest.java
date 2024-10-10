831157c4a94b944577dc0d8ee238f0a1ab11d6ec
aleung30 <aleung30@student.ubc.ca>
2024-10-09 19:38:36 -0700

    Added tests for Accomplishment class

---
+++ src/test/model/AccomplishmentTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class AccomplishmentTest {
+    private Accomplishment testAccomplishment;
+
+    @BeforeEach
+    void runBefore() {
+     testAccomplishment = new Accomplishment("Scored 100 on Math 101 midterm", "Oct 6, 2024");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Scored 100 on Math 101 midterm", testAccomplishment.getName());
+        assertEquals("Oct 6, 2024", testAccomplishment.getDate());
+    }
+
+    @Test
+    void testChangeName() {
+        testAccomplishment.changeName("Scored 90 on Math 101 midterm");
+        assertEquals("Scored 90 on Math 101 midterm", testAccomplishment.getName());
+    }
+    
+    @Test
+    void testChangeDate() {
+        testAccomplishment.changeDate("Oct 7, 2024");
+        assertEquals("Oct 7, 2024", testAccomplishment.getDate());
+    }
+    
+
+}
