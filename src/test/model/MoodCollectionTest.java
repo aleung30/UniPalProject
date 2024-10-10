8b041c8277177e397707bbefd2a0eb16423648d8
aleung30 <aleung30@student.ubc.ca>
2024-10-10 13:42:35 -0700

    Added additional test for MoodCollection class

--- src/test/model/MoodCollectionTest.java
+++ src/test/model/MoodCollectionTest.java
@@ -11,12 +11,14 @@ public class MoodCollectionTest {
     private MoodCollection testMoodCollection;
     private Mood testMood1;
     private Mood testMood2;
+    private Mood testMood3;
 
     @BeforeEach
     void runBefore() {
         testMoodCollection = new MoodCollection();
         testMood1 = new Mood("Happy because I passed math!", "POSITIVE", "Oct 10, 2024");
-        testMood2 = new Mood("Sad because I failed chemistry!", "NEGATIVE", "Oct 10, 2024");
+        testMood2 = new Mood("Bored", "NEUTRAL", "Oct 10, 2024");
+        testMood3 = new Mood("Sad because I failed chemistry!", "NEGATIVE", "Oct 10, 2024");
     }
 
     @Test
@@ -50,17 +52,26 @@ public class MoodCollectionTest {
         testMoodCollection.addMood(testMood2);
         testMoodCollection.removeMood(testMood1);
         assertEquals(1, testMoodCollection.moodSize());
-        testMoodCollection.removeMood(testMood1);
+        testMoodCollection.removeMood(testMood2);
         assertEquals(0, testMoodCollection.moodSize());
     }
 
     @Test
-    void testContainsMood() {
-        testMoodCollection.containsMood("Happy because I passed math!", "POSITIVE", "Oct 10, 2024");
+    void testContainsMoodName() {
+        testMoodCollection.containsMoodName("Happy because I passed math!");
         assertEquals("Happy because I passed math!", testMood1.getName());
         testMoodCollection.addMood(testMood1);
-        assertFalse(testMoodCollection.containsMood("Sad because I failed chemistry!", "NEGATIVE", "Oct 10, 2024"));
-        assertTrue(testMoodCollection.containsMood("Happy because I passed math!", "POSITIVE", "Oct 10, 2024"));
+        assertFalse(testMoodCollection.containsMoodName("Sad because I failed chemistry!"));
+        assertTrue(testMoodCollection.containsMoodName("Happy because I passed math!"));
+    }
+
+    @Test
+    void testContainsTypeName() {
+        testMoodCollection.containsMoodType("POSITIVE");
+        assertEquals("POSITIVE", testMood1.getType());
+        testMoodCollection.addMood(testMood1);
+        assertFalse(testMoodCollection.containsMoodType("NEUTRAL"));
+        assertTrue(testMoodCollection.containsMoodType("POSITIVE"));
     }
 
     @Test
@@ -68,7 +79,27 @@ public class MoodCollectionTest {
         testMoodCollection.addMood(testMood1);
         testMoodCollection.addMood(testMood2);
         testMoodCollection.sortMoodsByTypeName("POSITIVE");
-        assertEquals(testMood1, testMoodCollection.sortMoodsByTypeName("POSITIVE"));
+        assertTrue(testMoodCollection.containsMoodType("POSITIVE"));
     }
 
+    @Test
+    void testGetRecommendation() {
+        testMoodCollection.addMood(testMood1);
+        testMoodCollection.getRecommendation(testMood1);
+        assertEquals("POSITIVE", testMood1.getType());
+        assertEquals("That is great to hear!"
+                   + " Keep up the positive vibes with meditation, a light walk, or listening to upbeat songs!",
+                   testMoodCollection.getRecommendation(testMood1));
+        testMoodCollection.getRecommendation(testMood2);
+        assertEquals("NEUTRAL", testMood2.getType());
+        assertEquals("Feeling neutral is a great way to reflect on yourself!"
+                   + " You can engage in gentle stretching, read a book and journal, or watch a relaxing movie!",
+                    testMoodCollection.getRecommendation(testMood2));
+        testMoodCollection.getRecommendation(testMood3);
+        assertEquals("NEGATIVE", testMood3.getType());
+        assertEquals("It sounds like you are experiencing some overwhelming emotions!"
+                    + " How about some deep breathing exercises or spending time with friends and family?",
+                    testMoodCollection.getRecommendation(testMood3));
+
+    }
 }
