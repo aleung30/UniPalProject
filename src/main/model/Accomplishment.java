23d813de2870601391ec14da1f70789c9228c6f4
aleung30 <aleung30@student.ubc.ca>
2024-10-09 19:29:11 -0700

    Added specification of Accomplishment class

---
+++ src/main/model/Accomplishment.java
@@ -0,0 +1,43 @@
+package model;
+
+// Represents an accomplishment with a name and date
+public class Accomplishment {
+
+    /*
+     * REQUIRES: name and date have a non-zero length
+     * EFFECTS: constructs an accomplishment with a given name and date
+     * 
+     * 
+     */
+    public Accomplishment(String name, String date) {
+
+    }
+
+    /*
+     * REQUIRES: name input has a non-zero length
+     * MODIFIES: this
+     * EFFECTS: changes name of the accomplishment 
+     * 
+     */
+    public void changeName(String name) {
+
+    }
+
+    /*
+     * REQUIRES: date input has a non-zero length
+     * MODIFIES: this
+     * EFFECTS: changes date of the accomplishment 
+     * 
+     */
+    public String changeDate(String date) {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String getDate() {
+        return null;
+    }
+}
