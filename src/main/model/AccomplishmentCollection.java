9d637a81ff631c87ec89360533ddf1e525a9438b
aleung30 <aleung30@student.ubc.ca>
2024-10-09 23:23:08 -0700

    Added specification of AccomplishmentCollection class

---
+++ src/main/model/AccomplishmentCollection.java
@@ -0,0 +1,47 @@
+package model;
+
+// Represents a collection of Accomplishment objects
+public class AccomplishmentCollection {
+
+    /*
+     * EFFECTS: constructs an empty list that stores Accomplishment objects
+     */
+
+    public AccomplishmentCollection() {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds an accomplishment to the AccomplishmentCollection
+     */
+    public void addAccomplishment(Accomplishment accomplishment) {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: removes an accomplishment from the AccomplishmentCollection
+     */
+    public void removeAccomplishment(Accomplishment accomplishment) {
+
+    }
+
+    /*
+     * EFFECTS: returns true if name mathces the name of an accomplishment in the
+     * collection
+     */
+    public boolean containsAccomplishment(String name) {
+        return true;
+    }
+
+    /*
+     * REQUIRES: AccomplishmentCollection is not an empty list
+     * EFFECTS: returns the number of Accomplishment objects in the
+     * AccomplishmentCollection
+     */
+    public int accomplishmentSize() {
+        return 0;
+    }
+
+}
