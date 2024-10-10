226dc646c4cce4ec02cfdd2a6a84d222380c38b4
aleung30 <aleung30@student.ubc.ca>
2024-10-09 23:10:59 -0700

    Implemented Accomplishment

--- src/main/model/Accomplishment.java
+++ src/main/model/Accomplishment.java
@@ -3,39 +3,39 @@ package model;
 // Represents an accomplishment with a name and date
 public class Accomplishment {
 
+    private String name;
+    private String date;
+
     /*
      * REQUIRES: name and date have a non-zero length
      * EFFECTS: constructs an accomplishment with a given name and date
-     * 
-     * 
      */
     public Accomplishment(String name, String date) {
-
+        this.name = name;
+        this.date = date;
     }
 
     /*
      * REQUIRES: name input has a non-zero length
      * EFFECTS: changes name of the accomplishment 
-     * 
      */
     public void changeName(String name) {
-        
+        this.name = name;
     }
 
     /*
      * REQUIRES: date input has a non-zero length
      * EFFECTS: changes date of the accomplishment 
-     * 
      */
     public void changeDate(String date) {
-        
+        this.date = date;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public String getDate() {
-        return null;
+        return date;
     }
 }
