2c3b5b930429c1a51e7ac5214435207c8d127fe9
aleung30 <aleung30@student.ubc.ca>
2024-10-10 01:18:46 -0700

    Added specification of MoodCollection class

---
+++ src/main/model/MoodCollection.java
@@ -0,0 +1,58 @@
+package model;
+
+// Represents a collection of Mood objects
+public class MoodCollection {
+
+    /*
+     * EFFECTS: constructs an empty list to store Mood objects
+     */
+    public MoodCollection() {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds a Mood to the MoodCollection
+     * 
+     * 
+     */
+    public void addMood(Mood mood) {
+
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: removes a Mood from the MoodCollection
+     * 
+     * 
+     */
+    public void removeMood(Mood mood) {
+
+    }
+
+    /*
+     * EFFECTS: returns true if name matches the name of a mood in the collection
+     */
+    public boolean containsMood(String name) {
+        return true;
+    }
+
+    /*
+     * REQUIRES: MoodCollection is not an empty list
+     * EFFECTS: returns all the moods in the list that match the type name
+     */
+
+    public MoodCollection getMoodsByTypeName() {
+        return null;
+    }
+
+    /*
+     * REQUIRES: getMoods has at least one item in the list
+     * EFFECTS: returns the number of Mood objects in the moods list
+     * 
+     */
+    public int moodSize() {
+        return 0;
+    }
+
+}
