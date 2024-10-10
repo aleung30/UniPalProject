4bab726a55aaa8a3f6886b3bb32eb0628606ab7d
aleung30 <aleung30@student.ubc.ca>
2024-10-09 23:52:07 -0700

    Added tests for AccomplishmentCollection class

---
+++ src/test/model/AccomplishmentCollectionTest.java
@@ -0,0 +1,62 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AccomplishmentCollectionTest {
+    private Accomplishment testAccomplishment1;
+    private Accomplishment testAccomplishment2;
+    private AccomplishmentCollection testAccomplishmentList;
+
+    @BeforeEach
+    void runBefore() {
+        testAccomplishment1 = new Accomplishment("Scored 100 on MATH 101 midterm", "Oct 6, 2024");
+        testAccomplishment2 = new Accomplishment("Scored 100 on CPSC 121 midterm", "Oct 6, 2024");
+        testAccomplishmentList = new AccomplishmentCollection();
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(0, testAccomplishmentList.accomplishmentSize());
+    }
+
+    @Test
+    void testAddSingleAccomplishment() {
+        testAccomplishmentList.addAccomplishment(testAccomplishment1);
+        assertEquals(1, testAccomplishmentList.accomplishmentSize());
+    }
+
+    @Test
+    void testAddMultipleAccomplishments() {
+        testAccomplishmentList.addAccomplishment(testAccomplishment1);
+        assertEquals(1, testAccomplishmentList.accomplishmentSize());
+        testAccomplishmentList.addAccomplishment(testAccomplishment2);
+        assertEquals(2, testAccomplishmentList.accomplishmentSize());
+    }
+
+    @Test
+    void testRemoveSingleAccomplishment() {
+        testAccomplishmentList.removeAccomplishment(testAccomplishment1);
+        assertEquals(0, testAccomplishmentList.accomplishmentSize());
+    }
+
+    @Test
+    void testRemoveMultipleAccomplishments() {
+        testAccomplishmentList.addAccomplishment(testAccomplishment1);
+        testAccomplishmentList.addAccomplishment(testAccomplishment2);
+        testAccomplishmentList.removeAccomplishment(testAccomplishment1);
+        assertEquals(1, testAccomplishmentList.accomplishmentSize());
+        testAccomplishmentList.removeAccomplishment(testAccomplishment1);
+        assertEquals(0, testAccomplishmentList.accomplishmentSize());
+    }
+
+    @Test
+    void testContainsAccomplishment() {
+        testAccomplishmentList.addAccomplishment(testAccomplishment1);
+        assertTrue(testAccomplishmentList.containsAccomplishment("Scored 100 on MATH 101 midterm"));
+    }
+
+}
