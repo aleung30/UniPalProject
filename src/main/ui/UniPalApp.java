e369730f8759ac275f4b0e8fd910beaff9dceed7
aleung30 <aleung30@student.ubc.ca>
2024-10-11 11:22:55 -0700

    Added specification of UniPalApp (user interface)

---
+++ src/main/ui/UniPalApp.java
@@ -0,0 +1,64 @@
+package ui;
+
+// UniPal jounal application
+public class UniPalApp {
+
+    // EFFECTS: run the UniPal application
+    public UniPalApp() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runUniPal() {
+
+    }
+
+    // EFFECTS: lists different menu choices for user
+    private void showMenuChoices() {
+
+    }
+
+    // EFFECTS:
+    private void processCommand(String command) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes mood and accomplishment collections
+    private void initializeCollections() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: conducts adding an accomplishment to the collection
+    private void doAddAccomplishment() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: conducts adding a mood to the collection
+    private void doAddMood() {
+
+    }
+
+    // EFFECTS: prints out all accomplishments in the accomplishment collection
+    private void doViewAccomplishmentCollection() {
+
+    }
+
+    // EFFECTS: prints out all moods in the mood collection
+    private void doViewMoodCollection() {
+
+    }
+
+    // EFFECTS: conducts checking mood collection for a certain mood name
+    private void doCheckForMoodName() {
+
+    }
+
+    // EFFECTS: conducts suggesting relaxation methods based on mood type
+    private void doSuggestRelaxationMethods() {
+
+    }
+}
