f7b69521e759199211dc646a3c342b3d93d3683f
aleung30 <aleung30@student.ubc.ca>
2024-10-24 13:37:17 -0700

    Updated implementation of Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,15 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        new UniPalApp();
+        try {
+            new UniPalApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
-}
+      
+    }
+
