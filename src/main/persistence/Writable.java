465eec3ad42452c30429e2828854119a701c2301
aleung30 <aleung30@student.ubc.ca>
2024-10-24 13:13:50 -0700

    Added specification of Writable interface

---
+++ src/main/persistence/Writable.java
@@ -0,0 +1,10 @@
+package persistence;
+
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+public interface Writable {
+     // EFFECTS: returns this as JSON object
+
+}
+
+
