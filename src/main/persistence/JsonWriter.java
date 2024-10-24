4998efebae0de833655bbb34b791b90428522164
aleung30 <aleung30@student.ubc.ca>
2024-10-24 13:09:33 -0700

    Implemented JsonWriter class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -2,14 +2,22 @@ package persistence;
 
 import model.UniPalBoard;
 
+import org.json.JSONObject;
+
 import java.io.*;
 
-// Represents a writer that writes JSON representation of unipalboard to file
+// Referenced from the JsonSerialization Demo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+// Represents a writer that writes JSON representation of workroom to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
@@ -17,24 +25,25 @@ public class JsonWriter {
     // cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
-    // EFFECTS: writes JSON representation of unipalboard to file
+    // EFFECTS: writes JSON representation of unipal to file
     public void write(UniPalBoard up) {
-
+        JSONObject json = up.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-
+        writer.print(json);
     }
 }
