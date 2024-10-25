1b2478cf99b8bd0eaf9828c4f9cf02bd7bcfbb93
aleung30 <aleung30@student.ubc.ca>
2024-10-25 02:09:18 -0700

    Removed unnecessary import in UniPalBoardTest

--- src/test/model/UniPalBoardTest.java
+++ src/test/model/UniPalBoardTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.json.JSONArray;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
