1cc7fbd84ee009c8cf5b89989cdec44fab762f1b
aleung30 <aleung30@student.ubc.ca>
2024-10-25 02:12:56 -0700

    Fixed checkstyle errors for Writable and Main class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -5,8 +5,6 @@ import org.json.JSONObject;
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public interface Writable {
-     // EFFECTS: returns this as JSON object
-     JSONObject toJson();
+    // EFFECTS: returns this as JSON object
+    JSONObject toJson();
 }
-
-
