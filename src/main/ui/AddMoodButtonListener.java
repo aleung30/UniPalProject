4ad0e8712350717862cd0541d5b5a9573d3ee0e2
aleung30 <aleung30@student.ubc.ca>
2024-11-21 14:35:58 -0800

    Added specification of AddMoodButtonListener class

---
+++ src/main/ui/AddMoodButtonListener.java
@@ -0,0 +1,16 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when Add Mood Button is clicked
+public class AddMoodButtonListener implements ActionListener {
+
+    /*
+     * EFFECTS: adds mood name, type, and date to the Mood Collection
+     */
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
