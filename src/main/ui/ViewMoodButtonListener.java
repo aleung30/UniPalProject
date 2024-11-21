2dddc3bee43934446f17003bfdb922c68ae8af14
aleung30 <aleung30@student.ubc.ca>
2024-11-21 13:37:58 -0800

    Added specification of ViewMoodButtonListener class

---
+++ src/main/ui/ViewMoodButtonListener.java
@@ -0,0 +1,15 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when View Mood Button is clicked
+public class ViewMoodButtonListener implements ActionListener {
+
+    /*
+     * EFFECTS: creates a new JFrame to display user's moods
+     * and shows them in a table
+     */
+    public void actionPerformed(ActionEvent e) {
+    }
+}
