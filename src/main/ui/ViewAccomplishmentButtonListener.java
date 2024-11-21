b6963c41837dcec7fb29d626ea15332bf4526003
aleung30 <aleung30@student.ubc.ca>
2024-11-21 13:12:46 -0800

    Added specification of ViewAccomplishmentButtonListener class

---
+++ src/main/ui/ViewAccomplishmentButtonListener.java
@@ -0,0 +1,19 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+// Represents the action taken when View Accomplishment Button is clicked
+public class ViewAccomplishmentButtonListener implements ActionListener {
+
+
+    /*
+     * EFFECTS: creates a new JFrame to display user's accomplishments 
+     *          and shows them in a table
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      
+    }
+}
