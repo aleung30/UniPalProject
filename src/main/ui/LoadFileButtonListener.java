29d400d527411186c44663aea1c8d88bbaba82fc
aleung30 <aleung30@student.ubc.ca>
2024-11-21 14:46:50 -0800

    Added specification for LoadFileButtonListener and SaveFileButtonListener classes

---
+++ src/main/ui/LoadFileButtonListener.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoadFileButtonListener implements ActionListener {
+
+     /*
+     * EFFECTS: loads previous application from JSON file
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+   
+    }
+
+}
+
---
+++ src/main/ui/SaveFileButtonListener.java
@@ -0,0 +1,18 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when Save File Button is clicked
+public class SaveFileButtonListener implements ActionListener {
+
+
+     /*
+     * EFFECTS: saves current application to JSON file
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+   
+    }
+
+}
