0530ab09e0b5982459deff44d9ef915252a1e0bb
aleung30 <aleung30@student.ubc.ca>
2024-11-21 14:39:22 -0800

    Added specification of AddAccomplishmentButtonListener class

---
+++ src/main/ui/AddAccomplishmentButtonListener.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when Add Accomplishment Button is clicked
+public class AddAccomplishmentButtonListener implements ActionListener {
+
+    /*
+     * EFFECTS: adds accomplishment name and date to the Accomplishment Collection
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
