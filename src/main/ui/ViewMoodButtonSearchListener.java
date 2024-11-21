05e1e0a11fbd14d5531634f238568194a817dc1c
aleung30 <aleung30@student.ubc.ca>
2024-11-21 13:53:12 -0800

    Added specification of ViewMoodButtonSearchListener class

---
+++ src/main/ui/ViewMoodButtonSearchListener.java
@@ -0,0 +1,33 @@
+package ui;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class ViewMoodButtonSearchListener implements DocumentListener {
+
+    /*
+     * EFFECTS: called when text is inserted into the field
+     */
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+	
+	}
+
+    /*
+     * EFFECTS: called when text is removed from the field
+     */
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+	
+	}
+
+    /*
+     * EFFECTS: called when text is removed from the field
+     */
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+
+	}
+
+
+}
