1f6a98099a032ed41cbbff3b4819b15829624742
aleung30 <aleung30@student.ubc.ca>
2024-11-21 18:56:35 -0800

    Added implementation for LoadFileButtonListener and SaveFileButtonListener classes

--- src/main/ui/LoadFileButtonListener.java
+++ src/main/ui/LoadFileButtonListener.java
@@ -2,16 +2,41 @@ package ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import model.UniPalBoard;
+import persistence.JsonReader;
 
 public class LoadFileButtonListener implements ActionListener {
+    private JsonReader jsonReader;
+    private static final String JSON_STORE = "./data/unipalboard.json";
+    private UniPalBoard uniPal;
 
-     /*
+    /*
      * EFFECTS: loads previous application from JSON file
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+        uniPal = MainMenu.getUniPal();
+        jsonReader = new JsonReader(JSON_STORE);
+        loadUniPalBoard();
+
+    }
    
+    // Referenced from the JsonSerialization Demo
+    // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+    // MODIFIES: this
+    // EFFECTS: loads UniPalBoard from file
+    private void loadUniPalBoard() {
+        try {
+            uniPal = jsonReader.read();
+            MainMenu.setUniPal(uniPal);
+            JOptionPane.showMessageDialog(null, "Loaded " + uniPal.getName() + " from " + JSON_STORE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to read from file: " + JSON_STORE);
+        }
     }
 
 }
-
--- src/main/ui/SaveFileButtonListener.java
+++ src/main/ui/SaveFileButtonListener.java
@@ -2,17 +2,43 @@ package ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+
+import javax.swing.JOptionPane;
+
+import model.UniPalBoard;
+import persistence.JsonWriter;
 
 // Represents the action taken when Save File Button is clicked
 public class SaveFileButtonListener implements ActionListener {
-
-
-     /*
-     * EFFECTS: saves current application to JSON file
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-   
-    }
+    private JsonWriter jsonWriter;
+        private static UniPalBoard uniPal;
+        private static final String JSON_STORE = "./data/unipalboard.json";
+    
+        /*
+         * EFFECTS: saves current application to JSON file
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            uniPal = MainMenu.getUniPal();
+            jsonWriter = new JsonWriter(JSON_STORE);
+            saveUniPalBoard();
+        }
+    
+        /*
+         * Referenced from the JsonSerialization Demo
+         * https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+         * EFFECTS: saves UniPalBoard to file
+         */
+        public void saveUniPalBoard() {
+            try {
+                jsonWriter.open();
+                jsonWriter.write(uniPal);
+                jsonWriter.close();
+                JOptionPane.showMessageDialog(null, "Saved " + uniPal.getName() + " to " + JSON_STORE);
+            } catch (FileNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "Unable to write to file: " + JSON_STORE);
+            }
+        }
 
 }
