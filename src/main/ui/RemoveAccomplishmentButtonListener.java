02b5a6acaf67323ac180163f21841b24feb4795e
aleung30 <aleung30@student.ubc.ca>
2024-11-21 19:53:33 -0800

    Implemented RemoveAccomplishmentButtonListener and RemoveMoodButtonListener classes

--- src/main/ui/RemoveAccomplishmentButtonListener.java
+++ src/main/ui/RemoveAccomplishmentButtonListener.java
@@ -3,8 +3,16 @@ package ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import model.Accomplishment;
+import model.Mood;
+import model.UniPalBoard;
+
 // Represents the action taken when Remove Accomplishment Button is clicked
 public class RemoveAccomplishmentButtonListener implements ActionListener {
+    private static UniPalBoard uniPal;
 
     /*
      * EFFECTS: removes accomplishment name and date from the Accomplishment
@@ -12,7 +20,30 @@ public class RemoveAccomplishmentButtonListener implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+        uniPal = MainMenu.getUniPal();
+        String name = JOptionPane.showInputDialog("Enter the name of your accomplishment");
 
+        String date = JOptionPane.showInputDialog("Enter the date of your accomplishment");
+
+       
+        Accomplishment removeAccomplishment = null;
+        for (Accomplishment accomplishment : uniPal.getAccomplishmentCollection()) {
+            if (accomplishment.getName().equals(name) && accomplishment.getDate().equals(date)) {
+                removeAccomplishment = accomplishment;
+                break;
+            }
+        }
+
+        if (removeAccomplishment != null) {
+            uniPal.removeAccomplishment(removeAccomplishment);
+            JOptionPane.showMessageDialog(null, "You have successfully removed the accomplishment: " + removeAccomplishment.getName());
+        } else {
+            JOptionPane.showMessageDialog(null, "Accomplishment was not found in the collection, please try again.");
+        }
     }
-
 }
+        
+
+    
+
+
--- src/main/ui/RemoveMoodButtonListener.java
+++ src/main/ui/RemoveMoodButtonListener.java
@@ -3,15 +3,39 @@ package ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import model.Mood;
+import model.UniPalBoard;
+
 // Represents the action taken when Remove Mood Button is clicked
 public class RemoveMoodButtonListener implements ActionListener {
+    private static UniPalBoard uniPal;
 
     /*
      * EFFECTS: removes mood name, type, and date from the Accomplishment Collection
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+        uniPal = MainMenu.getUniPal();
+        String name = JOptionPane.showInputDialog("Enter the name of your mood");
+        String type = JOptionPane.showInputDialog("Enter your mood type");
+        String date = JOptionPane.showInputDialog("Enter the date of your mood");
 
+        Mood removeMood = null;
+        for (Mood mood : uniPal.getMoodCollection()) {
+            if (mood.getName().equals(name) && mood.getType().equals(type) && mood.getDate().equals(date)) {
+                removeMood = mood;
+                break;
+            }
+        }
+
+        if (removeMood != null) {
+            uniPal.removeMood(removeMood);
+            JOptionPane.showMessageDialog(null, "You have successfully removed the mood: " + removeMood.getName());
+        } else {
+            JOptionPane.showMessageDialog(null, "Mood was not found in the collection, please try again.");
+        }
     }
-
 }
