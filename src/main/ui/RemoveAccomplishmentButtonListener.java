ec3dd16971302b91cd1900ea795cf4b251f4b029
aleung30 <aleung30@student.ubc.ca>
2024-11-21 18:59:58 -0800

    Added specification of RemoveMoodButtonListener and RemoveAccomplishmentButtonListener classes

---
+++ src/main/ui/RemoveAccomplishmentButtonListener.java
@@ -0,0 +1,18 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when Remove Accomplishment Button is clicked
+public class RemoveAccomplishmentButtonListener implements ActionListener {
+
+    /*
+     * EFFECTS: removes accomplishment name and date from the Accomplishment
+     * Collection
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
---
+++ src/main/ui/RemoveMoodButtonListener.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Represents the action taken when Remove Mood Button is clicked
+public class RemoveMoodButtonListener implements ActionListener {
+
+    /*
+     * EFFECTS: removes mood name, type, and date from the Accomplishment Collection
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+}
